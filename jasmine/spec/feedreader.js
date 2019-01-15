1b04359c40a2f7547c7fed376ca1faeeaf7cd5a7
cljennings <46170669+cljennings@users.noreply.github.com>
2019-01-14 23:29:08 -0500

    allFeeds test suite completed.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,27 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('has defined url', function () {
+           allFeeds.forEach(function (element) {
+             const arrayItem = element.url;
+             expect(arrayItem).toBeDefined();
+             expect(arrayItem).not.toBe('');
+           })
+          });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('has defined name', function () {
+           allFeeds.forEach(function (element) {
+             const arrayItem = element.name;
+             expect(arrayItem).toBeDefined();
+             expect(arrayItem).not.toBe('');
+           })
+          });
+
     });
 
 
